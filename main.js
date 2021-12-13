0720768f0efdad579eabe36150867be2adf9faeb
Fry-Fr <68477278+Fry-Fr@users.noreply.github.com>
2021-12-12 21:57:11 -0500

    checks media_type for image or video

--- main.js
+++ main.js
@@ -8,26 +8,38 @@ root.appendChild(header);
 header.appendChild(title);
 header.appendChild(date);
 
+let apod;
 const main = document.createElement("main");
 const copyRight = document.createElement("h4");
-const apod = document.createElement("img");
 const explanation = document.createElement("p");
 
 root.appendChild(main);
 main.appendChild(copyRight);
-main.appendChild(apod);
-main.appendChild(explanation);
 
 function getAPOD() {
     fetch(`https://api.nasa.gov/planetary/apod?api_key=${'DEMO_KEY'}`)
-        .then(res => res.json())
-        .then(data => {
-            title.textContent = data.title;
-            date.textContent = data.date;
-            copyRight.textContent = data.copyright;
-            apod.alt = data.title;
-            apod.src = data.url;
-            explanation.textContent = data.explanation;
+    .then(res => res.json())
+    .then(data => {
+        title.textContent = data.title;
+        date.textContent = data.date;
+        copyRight.textContent = data.copyright;
+        explanation.textContent = data.explanation;
+        if (data.media_type === "image") {
+                apod = document.createElement("img");
+                apod.alt = data.title;
+                apod.src = data.url;
+                main.appendChild(apod);
+                main.appendChild(explanation);
+            }
+            if (data.media_type === "video") {
+                apod = document.createElement("iframe");
+                apod.height = '720px';
+                apod.width = '1220px';
+                apod.src = data.url + '&mute=1';
+                main.appendChild(apod);
+                main.appendChild(explanation);
+            }
+            console.log(data)
         })
         .catch(err => console.log(err))
 }
